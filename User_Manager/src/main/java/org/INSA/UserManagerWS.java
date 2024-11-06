53fea723a93c3d5e20ca8491c7fec4c7ef610d40
bfau21 <benoit@thefau.com>
2024-11-06 10:35:52 +0100

    SOAP addUser + removeUser

--- User_Manager/src/main/java/org/INSA/UserManagerWS.java
+++ User_Manager/src/main/java/org/INSA/UserManagerWS.java
@@ -12,27 +12,37 @@ public class UserManagerWS {
     String user = "projet_gei_062";
     String pswd = "Uph3Quie";
     @WebMethod(operationName="addUser")
-    public int addUser(@WebParam(name="username") String username) {
-        Créer un user
+    public void addUser(@WebParam(name="username") String username, @WebParam(name="type") int type, @WebParam(name="password") String password) {
         try (Connection connexion = DriverManager.getConnection(url, user, pswd)) {
-            String req = "INSERT INTO user (username, type, password) VALUES (?, ?, ?)";
+            String req = "INSERT INTO user (username, type, mdp) VALUES (?, ?, ?)";
             try (PreparedStatement statement = connexion.prepareStatement(req)) {
                 statement.setString(1, username);
                 statement.setInt(2, type);
                 statement.setString(3, password);
                 statement.executeUpdate();
-                System.out.println("Cléa");
+                System.out.println("OK");
             }
         } catch (SQLException e) {
-            e.getErrorCode();
-            System.out.println("Bobi");
+        	if (e.getErrorCode() == 1062) {  // Code d'erreur pour contrainte unique
+                System.out.println("Nom d'utilisateur déjà existant");
+            } else {
+                System.out.println("Erreur ajout user: " + e.getMessage());
+            }
         }
-        return 12;
     }
 
     @WebMethod(operationName="removeUser")
-    public int removeUser(@WebParam(name="id") int id) {
-        //Créer un user
-        return 13;
+    public void removeUser(@WebParam(name="username") String username) {
+    	try (Connection connexion = DriverManager.getConnection(url, user, pswd)) {
+            String req = "DELETE FROM user WHERE username = ?";
+            try (PreparedStatement statement = connexion.prepareStatement(req)) {
+                statement.setString(1, username);
+                statement.executeUpdate();
+                System.out.println("OK");
+            }
+        } catch (SQLException e) {
+            e.getErrorCode();
+            System.out.println("Erreur suppression user: " + e.getMessage());
+        }
     }
 }
