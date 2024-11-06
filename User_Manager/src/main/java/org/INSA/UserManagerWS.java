baace075db85dd4aac121228836212a5086c2122
bfau21 <benoit@thefau.com>
2024-11-06 09:51:26 +0100

    TEST ADD USER SOAP

--- User_Manager/src/main/java/org/INSA/UserManagerWS.java
+++ User_Manager/src/main/java/org/INSA/UserManagerWS.java
@@ -13,20 +13,20 @@ public class UserManagerWS {
     String pswd = "Uph3Quie";
     @WebMethod(operationName="addUser")
     public int addUser(@WebParam(name="username") String username) {
-        //Créer un user
-//        try (Connection connexion = DriverManager.getConnection(url, user, pswd)) {
-//            String req = "INSERT INTO user (username, type, password) VALUES (?, ?, ?)";
-//            try (PreparedStatement statement = connexion.prepareStatement(req)) {
-//                statement.setString(1, username);
-//                statement.setInt(2, type);
-//                statement.setString(3, password);
-//                statement.executeUpdate();
-//                System.out.println("Cléa");
-//            }
-//        } catch (SQLException e) {
-//            e.getErrorCode();
-//            System.out.println("Bobi");
-//        }
+        Créer un user
+        try (Connection connexion = DriverManager.getConnection(url, user, pswd)) {
+            String req = "INSERT INTO user (username, type, password) VALUES (?, ?, ?)";
+            try (PreparedStatement statement = connexion.prepareStatement(req)) {
+                statement.setString(1, username);
+                statement.setInt(2, type);
+                statement.setString(3, password);
+                statement.executeUpdate();
+                System.out.println("Cléa");
+            }
+        } catch (SQLException e) {
+            e.getErrorCode();
+            System.out.println("Bobi");
+        }
         return 12;
     }
 
